e3cff59760229cce53a90b6af2e3dbb6e1239995
GermanKos <kostiakovg@gmail.com>
2022-12-01 22:42:17 +0200

    incomplete user model

---
+++ Communion/Communion.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+namespace Communion.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; set; } = null!;
+    public byte[] PasswordHash { get; set; } = null!;
+    public byte[] PasswordSalt { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Bio { get; set; } = "Not specified.";
+    public string Gender { get; set; } = "Not specified";
+    public bool IsAdmin { get; set; } = false;
+    public List<string> comments { get; set; } = new List<string>(); // Change to list of Comments
+    public List<string> Posts { get; set; } = new List<string>();  // Change to list of Posts
+    public string? ProfilePicture { get; set; } // Change to ProfilePicture model
+    public DateTime? DateOfBirth { get; set; }
+    public DateTime RegistrationDate { get; set; }
+}
