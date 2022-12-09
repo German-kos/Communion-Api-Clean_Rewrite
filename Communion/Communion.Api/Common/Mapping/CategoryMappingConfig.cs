9f9e93265a4b6f1320b0cc1fea2929cb4ba8c337
German <KostiakovG@Gmail.com>
2022-12-09 20:32:23 +0200

    Commented out bad map

--- Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
+++ Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
@@ -1,5 +1,6 @@
 using Communion.Application.Categories.Commands.CreateCategory;
 using Communion.Contracts.Categories;
+using Communion.Domain.CategoryAggregate;
 using Mapster;
 
 namespace Communion.Api.Common.Mapping;
@@ -9,9 +10,13 @@ public class CategoryMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<(CreateCategoryRequest Request, string Username, string tempId, string tempUrl), CreateCategoryCommand>()
-        .Map(dest => dest.Username, src => src.Username)
-        .Map(dest => dest.BannerPublicId, src => src.tempId) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
-        .Map(dest => dest.BannerUrl, src => src.tempUrl) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
-        .Map(dest => dest, src => src.Request);
+            .Map(dest => dest.Username, src => src.Username)
+            .Map(dest => dest.BannerPublicId, src => src.tempId) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
+            .Map(dest => dest.BannerUrl, src => src.tempUrl) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
+            .Map(dest => dest, src => src.Request);
+
+        config.NewConfig<Category, CategoryResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value);
+        // .Map(dest => dest.Topics, src => src.Topics.Select(topic => topic.Id.Value));
     }
 }
