acca426e925488bc81e2bcbcc121e4ef408564de
German <KostiakovG@Gmail.com>
2022-12-09 20:39:11 +0200

    Repository DI & refactoring

--- Communion/Communion.Infrastructure/DependencyInjection.cs
+++ Communion/Communion.Infrastructure/DependencyInjection.cs
@@ -19,11 +19,20 @@ public static class DependencyInjection
         this IServiceCollection services,
         ConfigurationManager configuration)
     {
-        services.AddAuth(configuration);
+        services
+            .AddAuth(configuration)
+            .AddPersistance();
 
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
+        return services;
+    }
+
+    public static IServiceCollection AddPersistance(
+            this IServiceCollection services)
+    {
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
 
         return services;
     }
