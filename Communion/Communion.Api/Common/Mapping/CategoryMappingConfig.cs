7ed24e2f9fc2225e8c5719e5711413088add19bd
German <KostiakovG@Gmail.com>
2022-12-11 17:24:30 +0200

    Reconfigured mapping for banners

--- Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
+++ Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
@@ -11,10 +11,15 @@ public class CategoryMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<(CreateCategoryRequest Request, string Username, string tempId, string tempUrl), CreateCategoryCommand>()
+        config.NewConfig
+                <(CreateCategoryRequest Request,
+                    string Username,
+                    string BannerPublicId,
+                    string BannerUrl),
+                CreateCategoryCommand>()
             .Map(dest => dest.Username, src => src.Username)
-            .Map(dest => dest.BannerPublicId, src => src.tempId) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
-            .Map(dest => dest.BannerUrl, src => src.tempUrl) // Mapster cannot iterate with IFormFiles, add the Cloudinary service to upload images with an image service middleware
+            .Map(dest => dest.BannerPublicId, src => src.BannerPublicId)
+            .Map(dest => dest.BannerUrl, src => src.BannerUrl)
             .Map(dest => dest, src => src.Request);
 
         config.NewConfig<Category, CategoryResponse>()
