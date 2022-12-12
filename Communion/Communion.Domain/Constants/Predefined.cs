5702b338328bc7c531028346c0012cc8fee265af
German <KostiakovG@Gmail.com>
2022-12-12 14:41:15 +0200

    Added category constants

--- Communion/Communion.Domain/Constants/Predefined.cs
+++ Communion/Communion.Domain/Constants/Predefined.cs
@@ -3,4 +3,6 @@ namespace Communion.Domain.Constants;
 public static class Predefined
 {
     public const int MaxTopicAmount = 10;
+    public const int MinimumCategoryNameLength = 4;
+    public const int MaximumCategoryNameLength = 12;
 }
