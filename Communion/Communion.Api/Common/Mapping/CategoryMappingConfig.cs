d228f11034dfca9f9972ab20f7cf1cea8b639f47
German <KostiakovG@Gmail.com>
2022-12-12 11:53:59 +0200

    Created config for RenameCategory

--- Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
+++ Communion/Communion.Api/Common/Mapping/CategoryMappingConfig.cs
@@ -1,4 +1,5 @@
 using Communion.Application.Categories.Commands.CreateCategory;
+using Communion.Application.Categories.Commands.RenameCategory;
 using Communion.Contracts.Categories;
 using Communion.Domain.Category.Entities;
 using Communion.Domain.CategoryAggregate;
@@ -31,5 +32,13 @@ public class CategoryMappingConfig : IRegister
         config.NewConfig<Topic, TopicResponse>()
             .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.CategoryId, src => src.CategoryId.Value);
+
+        config.NewConfig
+                <(RenameCategoryRequest Request,
+                    string Username),
+                RenameCategoryCommand>()
+            .Map(dest => dest.CategoryId, src => new Guid(src.Request.CategoryId))
+            .Map(dest => dest.NewName, src => src.Request.NewName)
+            .Map(dest => dest.Username, src => src.Username);
     }
 }
