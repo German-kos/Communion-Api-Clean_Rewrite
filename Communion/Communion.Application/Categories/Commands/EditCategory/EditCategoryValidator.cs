e2ada57be50efeb90b7bff9161bf6b4063772f9e
German <KostiakovG@Gmail.com>
2022-12-12 14:38:51 +0200

    Created EditCategoryValidator

---
+++ Communion/Communion.Application/Categories/Commands/EditCategory/EditCategoryValidator.cs
@@ -0,0 +1,50 @@
+using System.Data;
+using System;
+using Communion.Domain.Constants;
+using FluentValidation;
+
+namespace Communion.Application.Categories.Commands.EditCategory;
+
+public class EditCategoryCommandValidator : AbstractValidator<EditCategoryCommand>
+{
+    public EditCategoryCommandValidator()
+    {
+        // CategoryId must be provided
+        RuleFor(r => r.CategoryId).NotEmpty();
+
+        // When NewBannerPublicId and NewBannerUrl are empty,
+        // NewName must be provided
+        When(r =>
+            string.IsNullOrEmpty(r.NewBannerPublicId)
+            && string.IsNullOrEmpty(r.NewBannerUrl),
+            () =>
+            {
+                RuleFor(r => r.NewName)
+                    .NotEmpty()
+                    .MinimumLength(Predefined.MinimumCategoryNameLength)
+                    .MaximumLength(Predefined.MaximumCategoryNameLength);
+            });
+
+        // When NewName is empty
+        // NewBannerPublicId and NewBannerUrl must be provided
+        When(r =>
+            string.IsNullOrEmpty(r.NewName),
+            () =>
+            {
+                RuleFor(r => r.NewBannerPublicId).NotEmpty();
+                RuleFor(r => r.NewBannerUrl).NotEmpty();
+            });
+
+        When(r =>
+            !string.IsNullOrEmpty(r.NewName)
+            && !string.IsNullOrEmpty(r.NewBannerPublicId)
+            && !string.IsNullOrEmpty(r.NewBannerUrl),
+            () =>
+            {
+                RuleFor(r => r.NewName)
+                    .NotEmpty()
+                    .MinimumLength(Predefined.MinimumCategoryNameLength)
+                    .MaximumLength(Predefined.MaximumCategoryNameLength);
+            });
+    }
+}
