fbf0f42398e65ee80fd098363ee31271b79e2712
German <KostiakovG@Gmail.com>
2022-12-13 14:32:39 +0200

    Created CreateTopicCommandHandler

---
+++ Communion/Communion.Application/Categories/Commands/CreateTopic/CreateTopicCommandHandler.cs
@@ -0,0 +1,35 @@
+using Communion.Domain.Common.DomainErrors;
+using Communion.Application.Common.Interfaces.Persistence;
+using Communion.Domain.CategoryAggregate;
+using ErrorOr;
+using MediatR;
+
+namespace Communion.Application.Categories.Commands.CreateTopic;
+
+public class CreateTopicCommandHandler
+            : IRequestHandler<CreateTopicCommand, ErrorOr<Category>>
+{
+    // Dependency Injections
+    private readonly ICategoryRepository _categoryRepository;
+    public CreateTopicCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    // Handler
+    public async Task<ErrorOr<Category>> Handle(CreateTopicCommand command, CancellationToken cancellationToken)
+    {
+        // Temporary await, remove when this method is made async
+        await Task.CompletedTask;
+
+        // Deconstruction
+        var (categoryId, topicName, username) = command;
+
+        var category = _categoryRepository.GetCategoryById(categoryId);
+
+        if (category is null)
+            return Errors.Category.CategoryNotFound;
+
+        return category.CreateTopic(topicName, username);
+    }
+}
