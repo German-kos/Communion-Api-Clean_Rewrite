03a66b508abaa309957f49ea41734d7148cc6674
German <KostiakovG@Gmail.com>
2022-12-13 16:44:01 +0200

    Added MinimumTopicsPerCategory

--- Communion/Communion.Domain/Constants/Predefined.cs
+++ Communion/Communion.Domain/Constants/Predefined.cs
@@ -7,4 +7,5 @@ public static class Predefined
     public const int MaximumCategoryNameLength = 12;
     public const int MinimumTopicNameLength = 4;
     public const int MaximumTopicNameLength = 16;
+    public const int MinimumTopicsPerCategory = 1;
 }
